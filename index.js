ac95a5bcf59a3842cb7c1d84eeb7be3207031f8d
Brian <bcantlose@gmail.com>
2023-03-29 18:37:58 -0500

    Control Flow Lab Complete

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){ 
+  if (someValue < 400)
+return "This one is on me!"
+
+else if (someValue <= 2000)
+
+return "That will be twenty bucks."
+
+else if (someValue <= 2500)
+
+return "I will gladly take your thirty bucks."
+
+else (someValue > 2500) 
+
+return "No can do."
+}
+function ternaryCheckCity(city){
+  
+ if (city === "NYC") 
+ return "Ok, sounds good." 
+ 
+ else if (city !== "NYC")
+ return "No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){
+        case "generous":
+          return "Thank you so much."
+        case "not as generous":
+          return "Thank you."
+        default:
+          return "Bye."
+  }
 }
